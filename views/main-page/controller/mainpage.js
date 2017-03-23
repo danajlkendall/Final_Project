27254f77ddef54a0c5529a5b61620a931c7aafe8
Dana Zukas <dana.zukas@gmail.com>
2017-03-23 19:43:56 -0400

    removed factory because we are no longer passing data back and forth between landing page and main page

--- views/main-page/controller/mainpage.js
+++ views/main-page/controller/mainpage.js
@@ -1,6 +1,17 @@
 var app = angular.module('mainApp');
 
-app.controller('mainPage',function($scope, dataFactory){
-  $scope.homData = dataFactory.sendData();
-  console.log($scope.homData);
+app.controller('mainPage',function($scope, $http){
+
+  var homData = {};
+
+  $http({
+      method: 'GET',
+      url: 'https://data.detroitmi.gov/resource/jut2-warj.json',
+      headers: {'X-App-Token': 'c9TllBUfGZDL2uOu9rgu1j0vw'}
+    }).then(function successCallBack(response) {
+      $scope.homData = response;
+      console.log(response);
+      console.log(homData);
+    });
+
 });
