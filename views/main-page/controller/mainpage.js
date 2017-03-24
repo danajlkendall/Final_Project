384e5ac8f0e9a433b51de5c0b5e27cb386b30a74
Dana Zukas <dana.zukas@gmail.com>
2017-03-23 21:38:15 -0400

    capturing JSON data in an array that has a name and we can access in the code - the original JSON data was un unnamed objects

--- views/main-page/controller/mainpage.js
+++ views/main-page/controller/mainpage.js
@@ -3,15 +3,24 @@ var app = angular.module('mainApp');
 app.controller('mainPage',function($scope, $http){
 
   var homData = {};
+  var allHomicides = [];
 
   $http({
       method: 'GET',
       url: 'https://data.detroitmi.gov/resource/jut2-warj.json',
       headers: {'X-App-Token': 'c9TllBUfGZDL2uOu9rgu1j0vw'}
     }).then(function successCallBack(response) {
-      $scope.homData = response;
-      console.log(response);
-      console.log(homData);
+      $scope.homData = response.data;
+
+      for (var i=0; i<$scope.homData.length; i++){
+        var homicide = {
+          precinct: $scope.homData[i].precinct,
+          date: $scope.homData[i].incidentdate
+        }
+        allHomicides.push(homicide);
+      };
     });
+
+    console.log(allHomicides);
 
 });
