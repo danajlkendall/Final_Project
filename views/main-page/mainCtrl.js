ce8290dc3d18baa5e15bc352a6a135582dfb9fd9
Dana Zukas <dana.zukas@gmail.com>
2017-04-01 14:54:00 -0400

    fixing search bar on map

--- views/main-page/mainCtrl.js
+++ views/main-page/mainCtrl.js
@@ -441,70 +441,69 @@ app.controller('mainPage',function($scope, $http){
         PrecinctTwelvePoly.setMap(map);
 
         // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+          var input = document.getElementById('pac-input');
+          var searchBox = new google.maps.places.SearchBox(input);
+          map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+          // Bias the SearchBox results towards current map's viewport.
+          map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+          });
 
-        //Creats the Search Box for the Google Map
-        function initAutocomplete() {
+          //Createss the Search Box for the Google Map
+          function initAutocomplete() {
 
-          var markers = [];
-          // Listen for the event fired when the user selects a prediction and retrieve
-          // more details for that place.
-          searchBox.addListener('places_changed', function() {
-            var places = searchBox.getPlaces();
-            var input = document.getElementById('pac-input');
-            var searchBox = new google.maps.places.SearchBox(input);
-            map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+            var markers = [];
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener('places_changed', function() {
+              var places = searchBox.getPlaces();
+              var input = document.getElementById('pac-input');
 
-            if (places.length == 0) {
-              return;
-            }
 
-            // Clear out the old markers.
-            markers.forEach(function(marker) {
-              marker.setMap(null);
-            });
-            markers = [];
-
-            // For each place, get the icon, name and location.
-            var bounds = new google.maps.LatLngBounds();
-            places.forEach(function(place) {
-              if (!place.geometry) {
-                console.log("Returned place contains no geometry");
+              if (places.length == 0) {
                 return;
               }
 
-              // Create a marker for each place.
-              markers.push(new google.maps.Marker({
-                map: map,
-                icon: new google.maps.MarkerImage('http://www.clker.com/cliparts/B/B/1/E/y/r/marker-pin-google.svg',   //Precinct Shield Icon Source Location
-                null, null, null, new google.maps.Size(60,60)),
-                title: place.name,
-                position: place.geometry.location
-              }));
+              // Clear out the old markers.
+              markers.forEach(function(marker) {
+                marker.setMap(null);
+              });
+              markers = [];
 
-              if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-              } else {
-                bounds.extend(place.geometry.location);
-              }
+              // For each place, get the icon, name and location.
+              var bounds = new google.maps.LatLngBounds();
+              places.forEach(function(place) {
+                if (!place.geometry) {
+                  console.log("Returned place contains no geometry");
+                  return;
+                }
+
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                  map: map,
+                  icon: new google.maps.MarkerImage('http://www.clker.com/cliparts/B/B/1/E/y/r/marker-pin-google.svg',   //Precinct Shield Icon Source Location
+                  null, null, null, new google.maps.Size(60,60)),
+                  title: place.name,
+                  position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+                } else {
+                  bounds.extend(place.geometry.location);
+                }
+              });
+              map.fitBounds(bounds);
             });
-            map.fitBounds(bounds);
-          });
 
-        }  //Closed out google maps search box
+          }  //Closed out google maps search box
 
-        initAutocomplete();
+          initAutocomplete();
+
+
+        });
 
 
       });
-
-
-    });
