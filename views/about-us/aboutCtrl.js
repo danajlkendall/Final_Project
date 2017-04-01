e29e59b622544daedf322de11fcc444a3281f9a0
Dana Zukas <dana.zukas@gmail.com>
2017-04-01 14:24:10 -0400

    edited html on about page directive template

--- views/about-us/aboutCtrl.js
+++ views/about-us/aboutCtrl.js
@@ -14,20 +14,20 @@ app.controller('aboutUs',function($scope, $http){
     {
       img: 'http://i.imgur.com/NxJsfNd.png',
       name: 'JOHN',
-      fact: 'Throughout the department John is known by the code name “Whispers”.',
-      why: 'John is learning coding as part of an exploratory process for the Detroit Police Department, to evaluate how coding can be applied within law enforcement.'
+      fact: 'Throughout the department John is known by the code name “Whispers”',
+      why: 'John is learning coding as part of an exploratory process for the Detroit Police Department, to evaluate how coding can be applied within law enforcement'
     },
     {
       img: 'http://i.imgur.com/ojCWASa.png',
       name: 'KARL',
-      fact: 'As a hobby Karl trains working dogs.',
-      why: 'Karl wants to build things that will shape the future.'
+      fact: 'As a hobby, Karl trains working dogs',
+      why: 'Karl wants to build things that will shape the future'
     },
     {
       img: 'http://i.imgur.com/RosO8JR.png',
       name: 'DANA',
-      fact: 'When she was 6 years old Dana rode her bike into a pool.',
-      why: 'hkjljkljjjjlj'
+      fact: 'When she was 6 years old, Dana accidentally rode her bike into a swimming pool',
+      why: "Dana's full-time job is to explain technology to customers, so it's about time she learns how the tool she trains people on is built"
     }
   ];
 });
